6049b343a4150551134b0bbe4c971c01ee5140d1
zohaibcodes9642 <zohaib.mustafa@gomotive.com>
2024-08-19 21:01:27 +0500

    .

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -2,11 +2,14 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 import os
+import pymysql
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
+pymysql.install_as_MySQLdb()
 
 LOGGING = {
     "version": 1,
